7a03311cc8f76690227d6a074d182726d303117d
Art <d.art.shishkin@gmail.com>
2020-07-04 13:32:45 +0300

    Init

---
+++ src/main/java/com/artarkatesoft/artsfgrecipeproject/ArtSfgRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.artsfgrecipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSfgRecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtSfgRecipeProjectApplication.class, args);
+    }
+
+}
