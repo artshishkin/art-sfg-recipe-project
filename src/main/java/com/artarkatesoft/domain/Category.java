dc1e6b2ed0fe7d789465328b453ac878dcfe6c75
admin <d.art.shishkin@gmail.com>
2020-07-06 23:04:21 +0300

    137 art-sfg-recipe-project-5 Many to Many JPA Relationship (closes #137)

---
+++ src/main/java/com/artarkatesoft/domain/Category.java
@@ -0,0 +1,22 @@
+package com.artarkatesoft.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String categoryName;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+}
--- src/main/java/com/artarkatesoft/domain/Recipe.java
+++ src/main/java/com/artarkatesoft/domain/Recipe.java
@@ -34,4 +34,11 @@ public class Recipe {
     @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
     private Set<Ingredient> ingredients;
 
+    @ManyToMany
+//    @JoinTable
+//    @JoinTable(name = "recipe_category",
+//            joinColumns = @JoinColumn(name = "recipe_id"),
+//            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
 }
