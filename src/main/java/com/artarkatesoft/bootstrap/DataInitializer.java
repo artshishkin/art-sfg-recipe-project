cc511a6e1a6ee1f29f1e66d9a3771da0a8bea04f
admin <d.art.shishkin@gmail.com>
2020-07-07 13:23:31 +0300

    141 refactor DataInitializer (added Directions and modified Notes) (art-sfg-recipe-project-6)

--- src/main/java/com/artarkatesoft/bootstrap/DataInitializer.java
+++ src/main/java/com/artarkatesoft/bootstrap/DataInitializer.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Supplier;
@@ -40,13 +39,18 @@ public class DataInitializer implements CommandLineRunner {
         recipe.setCookTime(0);
         recipe.setDescription("How to Make Perfect Guacamole Recipe");
         recipe.setServings(4);
-        recipe.setDifficulty(Difficulty.MEDIUM);
+        recipe.setDifficulty(Difficulty.EASY);
         recipe.setUrl("https://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        recipe.setDirections("1 Cut the avocado, remove flesh: Cut the avocados in half. Remove...\n" +
+                "2 Mash with a fork: Using a fork, roughly mash the avocado. \n" +
+                "3 Add salt, lime juice, and the rest: Sprinkle with salt and lime (or lemon) juice. \n" +
+                "4 Serve: Serve immediately, or if making a few hours ahead, place plastic wrap on the " +
+                "surface of the guacamole and press down to cover it and to prevent air reaching it.");
 
         Notes notes = new Notes();
-        notes.setNotes("Be careful handling chiles if using." +
-                " Wash your hands thoroughly after handling and do not touch" +
-                " your eyes or the area near your eyes with your hands for several hours.");
+        notes.setNotes("The best guacamole keeps it simple: just ripe avocados, salt, a squeeze of lime, " +
+                "onions, chiles, cilantro, and some chopped tomato. Serve it as a dip at your next party " +
+                "or spoon it on top of tacos for an easy dinner upgrade.");
 
         recipe.setNotes(notes);
         notes.setRecipe(recipe);
@@ -54,18 +58,20 @@ public class DataInitializer implements CommandLineRunner {
         Set<Ingredient> ingredients = new HashSet<>();
         recipe.setIngredients(ingredients);
 
-        UnitOfMeasure unit = unitOfMeasureRepository.findByDescription("Unit").orElseThrow(expectedUomNotFound);
-        UnitOfMeasure teaspoon = unitOfMeasureRepository.findByDescription("Teaspoon").orElseThrow(expectedUomNotFound);
-        UnitOfMeasure tablespoon = unitOfMeasureRepository.findByDescription("Tablespoon").orElseThrow(expectedUomNotFound);
-        UnitOfMeasure cup = unitOfMeasureRepository.findByDescription("Cup").orElseThrow(expectedUomNotFound);
+        UnitOfMeasure unitUom = unitOfMeasureRepository.findByDescription("Unit").orElseThrow(expectedUomNotFound);
+        UnitOfMeasure teaspoonUom = unitOfMeasureRepository.findByDescription("Teaspoon").orElseThrow(expectedUomNotFound);
+        UnitOfMeasure tablespoonUom = unitOfMeasureRepository.findByDescription("Tablespoon").orElseThrow(expectedUomNotFound);
+        UnitOfMeasure cupUom = unitOfMeasureRepository.findByDescription("Cup").orElseThrow(expectedUomNotFound);
 
-        ingredients.add(new Ingredient(recipe, "2 ripe avocados", valueOf(2), unit));
-        ingredients.add(new Ingredient(recipe, "1/4 teaspoon of salt, more to taste", valueOf(0.25), teaspoon));
-        ingredients.add(new Ingredient(recipe, "1 tablespoon fresh lime juice or lemon juice", valueOf(1), tablespoon));
-        ingredients.add(new Ingredient(recipe, "2 tablespoons to 1/4 cup of minced red onion or thinly sliced green onion", valueOf(0.25), cup));
+        ingredients.add(new Ingredient(recipe, "2 ripe avocados", valueOf(2), unitUom));
+        ingredients.add(new Ingredient(recipe, "1/4 teaspoon of salt, more to taste", valueOf(0.25), teaspoonUom));
+        ingredients.add(new Ingredient(recipe, "1 tablespoon fresh lime juice or lemon juice", valueOf(1), tablespoonUom));
+        ingredients.add(new Ingredient(recipe, "2 tablespoons to 1/4 cup of minced red onion or thinly sliced green onion", valueOf(0.25), cupUom));
 
         Category mexican = categoryRepository.findByDescription("Mexican").orElseThrow(expectedCategoryNotFound);
-        recipe.setCategories(Collections.singleton(mexican));
+        Category american = categoryRepository.findByDescription("American").orElseThrow(expectedCategoryNotFound);
+        recipe.getCategories().add(mexican);
+        recipe.getCategories().add(american);
         return recipe;
     }
 
@@ -77,13 +83,16 @@ public class DataInitializer implements CommandLineRunner {
         recipe.setServings(6);
         recipe.setDifficulty(Difficulty.HARD);
         recipe.setUrl("https://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
+        recipe.setDirections("1 Prepare a gas or charcoal grill for medium-high, direct heat.\n"+
+                "2 Make the marinade and coat the chicken: In a large bowl, stir together\n" +
+                "3 Grill the chicken: Grill the chicken for 3 to 4 minutes per side, \n" +
+                "4 Warm the tortillas: Place each tortilla on the grill or on a \n" +
+                "5 Assemble the tacos: Slice the chicken into strips. ");
 
         Notes notes = new Notes();
-        notes.setNotes("Look for ancho chile powder with the Mexican" +
-                " ingredients at your grocery store, on buy it online. " +
-                "(If you can't find ancho chili powder, you replace the ancho " +
-                "chili, the oregano, and the cumin with 2 1/2 tablespoons regular " +
-                "chili powder, though the flavor won't be quite the same.)");
+        notes.setNotes("Spicy grilled chicken tacos! Quick marinade, then grill. " +
+                "Ready in about 30 minutes. Great for a quick weeknight dinner, " +
+                "backyard cookouts, and tailgate parties");
 
         recipe.setNotes(notes);
         notes.setRecipe(recipe);
@@ -91,21 +100,18 @@ public class DataInitializer implements CommandLineRunner {
         Set<Ingredient> ingredients = new HashSet<>();
         recipe.setIngredients(ingredients);
 
-        UnitOfMeasure unit = unitOfMeasureRepository.findByDescription("Unit").orElseThrow(expectedUomNotFound);
-        UnitOfMeasure teaspoon = unitOfMeasureRepository.findByDescription("Teaspoon").orElseThrow(expectedUomNotFound);
-        UnitOfMeasure tablespoon = unitOfMeasureRepository.findByDescription("Tablespoon").orElseThrow(expectedUomNotFound);
-        UnitOfMeasure cup = unitOfMeasureRepository.findByDescription("Cup").orElseThrow(expectedUomNotFound);
+        UnitOfMeasure teaspoonUom = unitOfMeasureRepository.findByDescription("Teaspoon").orElseThrow(expectedUomNotFound);
+        UnitOfMeasure tablespoonUom = unitOfMeasureRepository.findByDescription("Tablespoon").orElseThrow(expectedUomNotFound);
 
-        ingredients.add(new Ingredient(recipe, "2 tablespoons ancho chili powder", valueOf(2), tablespoon));
-        ingredients.add(new Ingredient(recipe, "1 teaspoon dried oregano", valueOf(1), teaspoon));
-        ingredients.add(new Ingredient(recipe, "1 teaspoon dried cumin", valueOf(1), teaspoon));
-        ingredients.add(new Ingredient(recipe, "1 teaspoon sugar", valueOf(1), teaspoon));
+        ingredients.add(new Ingredient(recipe, "2 tablespoons ancho chili powder", valueOf(2), tablespoonUom));
+        ingredients.add(new Ingredient(recipe, "1 teaspoon dried oregano", valueOf(1), teaspoonUom));
+        ingredients.add(new Ingredient(recipe, "1 teaspoon dried cumin", valueOf(1), teaspoonUom));
+        ingredients.add(new Ingredient(recipe, "1 teaspoon sugar", valueOf(1), teaspoonUom));
 
-        recipe.setCategories(new HashSet<>());
+
         Category mexican = categoryRepository.findByDescription("Mexican").orElseThrow(expectedCategoryNotFound);
-        recipe.getCategories().add(mexican);
-
         Category fastFood = categoryRepository.findByDescription("Fast Food").orElseThrow(expectedCategoryNotFound);
+        recipe.getCategories().add(mexican);
         recipe.getCategories().add(fastFood);
 
         return recipe;
