bf16e59c61ca0e3407e2855069d1eaf1cd684d9a
admin <d.art.shishkin@gmail.com>
2020-07-09 10:45:37 +0300

    179 Maven Failsafe plugin

--- src/test/java/com/artarkatesoft/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/artarkatesoft/repositories/UnitOfMeasureRepositoryIT.java
@@ -2,11 +2,9 @@ package com.artarkatesoft.repositories;
 
 import com.artarkatesoft.domain.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.Optional;
 
@@ -25,7 +23,6 @@ class UnitOfMeasureRepositoryIT {
     }
 
     @Test
-    @DirtiesContext
     void findByDescriptionTablespoon() {
 
         String descriptionToFind = "Tablespoon";
@@ -35,7 +32,6 @@ class UnitOfMeasureRepositoryIT {
         assertThat(uom.getDescription()).isEqualTo(descriptionToFind);
     }
     @Test
-    @DirtiesContext
     void findByDescriptionTeaspoon() {
         String descriptionToFind = "Teaspoon";
         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription(descriptionToFind);
@@ -43,7 +39,6 @@ class UnitOfMeasureRepositoryIT {
         assertThat(uomOptional.get().getDescription()).isEqualTo(descriptionToFind);
     }
     @Test
-    @DirtiesContext
     void findByDescriptionCup() {
         String descriptionToFind = "Cup";
         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription(descriptionToFind);
@@ -57,14 +52,14 @@ class UnitOfMeasureRepositoryIT {
         assertTrue(uomOptional.isPresent());
         assertThat(uomOptional.get().getDescription()).isEqualTo(descriptionToFind);
     }
-    @RepeatedTest(10)
+    @Test
     void findByDescriptionOunce() {
         String descriptionToFind = "Ounce";
         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription(descriptionToFind);
         assertTrue(uomOptional.isPresent());
         assertThat(uomOptional.get().getDescription()).isEqualTo(descriptionToFind);
     }
-    @RepeatedTest(10)
+    @Test
     void findByDescriptionDash() {
         String descriptionToFind = "Dash";
         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription(descriptionToFind);
