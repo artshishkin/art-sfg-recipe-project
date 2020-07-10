44850a0fc45655cb43edd1ded38bbdbd6a37c65c
Art <d.art.shishkin@gmail.com>
2020-07-10 15:14:19 +0300

    197 Created Command Objects #15 art-sfg-recipe-project-15 197 Creating Command Objects and Type Convertions

---
+++ src/main/java/com/artarkatesoft/commands/CategoryCommand.java
@@ -0,0 +1,11 @@
+package com.artarkatesoft.commands;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryCommand {
+    private Long id;
+    private String description;
+}
---
+++ src/main/java/com/artarkatesoft/commands/NotesCommand.java
@@ -0,0 +1,11 @@
+package com.artarkatesoft.commands;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NotesCommand {
+    private Long id;
+    private String notes;
+}
