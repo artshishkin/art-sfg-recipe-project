4fa7d1f9a9cf0ded53f5bee1060b6cf317fc7fc9
Art <d.art.shishkin@gmail.com>
2020-07-11 11:43:19 +0300

    art-sfg-recipe-project-16 198 Create a Recipe (closes #16)

--- src/main/java/com/artarkatesoft/controllers/RecipeController.java
+++ src/main/java/com/artarkatesoft/controllers/RecipeController.java
@@ -1,12 +1,12 @@
 package com.artarkatesoft.controllers;
 
+import com.artarkatesoft.commands.RecipeCommand;
 import com.artarkatesoft.domain.Recipe;
 import com.artarkatesoft.services.RecipeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("recipe")
@@ -21,4 +21,27 @@ public class RecipeController {
         model.addAttribute("recipe", recipe);
         return "recipe/show";
     }
+
+    @GetMapping
+    public String showForm(Model model) {
+        Long id = 1L;
+        Recipe recipe = recipeService.getById(id);
+        model.addAttribute("recipe", recipe);
+
+        return "recipe/recipeform";
+    }
+
+    @RequestMapping("new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    public String createOrUpdate(@ModelAttribute("recipe") RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/"+savedRecipeCommand.getId();
+    }
+
+
 }
