2a573225b8418fc994f81711134c16b54d8db498
admin <d.art.shishkin@gmail.com>
2020-07-12 00:13:51 +0300

    #21 202 Show Ingredient - Changed IngredientServiceImpl to use RecipeRepository

--- src/main/java/com/artarkatesoft/services/IngredientServiceImpl.java
+++ src/main/java/com/artarkatesoft/services/IngredientServiceImpl.java
@@ -1,7 +1,9 @@
 package com.artarkatesoft.services;
 
 import com.artarkatesoft.commands.IngredientCommand;
-import com.artarkatesoft.commands.RecipeCommand;
+import com.artarkatesoft.converters.IngredientToIngredientCommandConverter;
+import com.artarkatesoft.domain.Recipe;
+import com.artarkatesoft.repositories.RecipeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,15 +11,20 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class IngredientServiceImpl implements IngredientService {
 
-    private final RecipeService recipeService;
+    private final RecipeRepository recipeRepository;
+    private final IngredientToIngredientCommandConverter toIngredientCommandConverter;
 
     @Override
     public IngredientCommand findIngredientCommandByIdAndRecipeId(Long id, Long recipeId) {
-        RecipeCommand recipeCommand = recipeService.getCommandById(recipeId);
-        return recipeCommand.getIngredients()
+        Recipe recipe = recipeRepository
+                .findById(recipeId)
+                .orElseThrow(() -> new RuntimeException("Recipe not found"));
+
+        return recipe.getIngredients()
                 .stream()
                 .filter(ingredient -> ingredient.getId().equals(id))
                 .findFirst()
+                .map(toIngredientCommandConverter::convert)
                 .orElseThrow(() -> new RuntimeException("Ingredient not Found"));
     }
 }
--- src/test/java/com/artarkatesoft/services/IngredientServiceImplTest.java
+++ src/test/java/com/artarkatesoft/services/IngredientServiceImplTest.java
@@ -1,22 +1,26 @@
 package com.artarkatesoft.services;
 
 import com.artarkatesoft.commands.IngredientCommand;
-import com.artarkatesoft.commands.RecipeCommand;
+import com.artarkatesoft.converters.IngredientToIngredientCommandConverter;
+import com.artarkatesoft.domain.Ingredient;
+import com.artarkatesoft.domain.Recipe;
+import com.artarkatesoft.repositories.RecipeRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Optional;
 import java.util.stream.LongStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 class IngredientServiceImplTest {
@@ -25,26 +29,38 @@ class IngredientServiceImplTest {
     IngredientServiceImpl ingredientService;
 
     @Mock
-    RecipeService recipeService;
+    RecipeRepository recipeRepository;
+    @Mock
+    IngredientToIngredientCommandConverter toIngredientCommandConverter;
+    @Captor
+    ArgumentCaptor<Ingredient> ingredientCaptor;
 
     @Test
     void findIngredientCommandByIdAndRecipeId() {
         //given
         Long id = 2L;
         Long recipeId = 1L;
-        RecipeCommand recipeCommand = new RecipeCommand();
-        Set<IngredientCommand> ingredientCommands =
-                LongStream
-                        .rangeClosed(1, 5)
-                        .mapToObj(i -> new IngredientCommand(i, "desc" + i, null, null))
-                        .collect(Collectors.toSet());
-        recipeCommand.setIngredients(ingredientCommands);
-        given(recipeService.getCommandById(anyLong())).willReturn(recipeCommand);
+        Recipe recipe = new Recipe();
+
+        LongStream.rangeClosed(1, 5)
+                .mapToObj(this::createFakeIngredient)
+                .forEach(recipe::addIngredient);
+
+        given(recipeRepository.findById(anyLong())).willReturn(Optional.of(recipe));
+        given(toIngredientCommandConverter.convert(any(Ingredient.class))).willReturn(new IngredientCommand());
         //when
         IngredientCommand ingredientCommand = ingredientService.findIngredientCommandByIdAndRecipeId(id, recipeId);
         //then
-        then(recipeService).should().getCommandById(eq(recipeId));
-        assertThat(ingredientCommand.getId()).isEqualTo(id);
+        then(recipeRepository).should().findById(eq(recipeId));
+        then(toIngredientCommandConverter).should(times(1)).convert(ingredientCaptor.capture());
+        assertThat(ingredientCaptor.getValue().getId()).isEqualTo(id);
 
+    }
+
+    private Ingredient createFakeIngredient(Long id) {
+        Ingredient ingredient = new Ingredient();
+        ingredient.setId(id);
+        ingredient.setDescription("Desc" + id);
+        return ingredient;
     }
 }
